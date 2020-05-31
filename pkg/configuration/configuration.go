6ec71e0b4b7153839f7edfdd8037f9ba0994d6ac
Keith Craley <kcral21@gmail.com>
2020-05-31 03:00:51 -0400

    add prefix to the configuration for the bot to listen for

--- pkg/configuration/configuration.go
+++ pkg/configuration/configuration.go
@@ -1,7 +1,13 @@
 package configuration
 
+const (
+	// DefaultPrefix is the default prefix which the bot listens to
+	DefaultPrefix = "!octane"
+)
+
 // Configuration contains the application configuration settings
 type Configuration struct {
+	Prefix  string ``
 	Token   string ``
 	Verbose bool   ``
 }
