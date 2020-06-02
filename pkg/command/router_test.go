c204899a42230e48a280158dc05c05c06c99eacc
Keith Craley <kcral21@gmail.com>
2020-06-01 23:55:27 -0400

    test creating an empty router

--- pkg/command/router_test.go
+++ pkg/command/router_test.go
@@ -8,20 +8,48 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-var (
-	routerTestCases = map[string]struct {
+func TestNewRouter(t *testing.T) {
+	// Test cases for creating a new Router
+	var routerTestCases = map[string]struct {
 		prefix   string
 		expected *command.Router
 	}{
 		"defaultPrefix": {prefix: "!octane", expected: &command.Router{Prefix: "!octane"}},
 		"customPrefix":  {prefix: "!custom", expected: &command.Router{Prefix: "!custom"}},
 	}
-)
 
-func TestNewRouter(t *testing.T) {
 	for name, test := range routerTestCases {
 		t.Run(name, func(t *testing.T) {
 			actual := command.NewRouter(test.prefix)
+			diff := cmp.Diff(test.expected, actual)
+			if diff != "" {
+				t.Fatalf(diff)
+			}
+		})
+	}
+}
+
+func TestRegisterCommand(t *testing.T) {
+	// Test cases for registering a new Command
+	var registerCommandTestCases = map[string]struct {
+		prefix     string
+		newCommand *command.Command
+		expected   *command.Router
+	}{
+		"emptyRouter": {
+			prefix:     "!octane",
+			newCommand: &command.Command{},
+			expected: &command.Router{
+				Prefix:   "!octane",
+				Commands: []*command.Command{{}},
+			},
+		},
+	}
+
+	for name, test := range registerCommandTestCases {
+		t.Run(name, func(t *testing.T) {
+			actual := command.NewRouter(test.prefix)
+			actual.RegisterCommand(test.newCommand)
 			diff := cmp.Diff(test.expected, actual)
 			if diff != "" {
 				t.Fatalf(diff)
