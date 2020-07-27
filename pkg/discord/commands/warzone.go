e37741812765e0dd45d1463a437de4964d343c57
Keith Craley <kcral21@gmail.com>
2020-07-27 01:38:45 -0400

    pull profile information using hardcoded values

--- pkg/discord/commands/warzone.go
+++ pkg/discord/commands/warzone.go
@@ -1,8 +1,15 @@
 package commands
 
 import (
-	"github.com/bwmarrin/discordgo"
+	"errors"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/kcraley/octane-go/pkg/callofduty"
 	"github.com/kcraley/octane-go/pkg/command"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 // WarzoneCmd is the bot command to lookup Call of Duty Warzone players
@@ -16,5 +23,67 @@ var WarzoneCmd = &command.Command{
 
 // warzoneCmdFunc is the handler function for looking up Call of Duty Warzone players
 func warzoneCmdFunc(s *discordgo.Session, m *discordgo.Message) error {
-	return nil
+	// Login to the Call of Duty APIs if credentials are set
+	if os.Getenv("COD_USERNAME") != "" && os.Getenv("COD_PASSWORD") != "" {
+		// Create a client to interact with the Call of Duty APIs
+		codClient := callofduty.NewClient()
+		err := codClient.Login(os.Getenv("COD_USERNAME"), os.Getenv("COD_PASSWORD"))
+		if err != nil {
+			return err
+		}
+
+		playerProfile, err := codClient.GetProfile("mw", "battle", "CoaXeD%231353", "wz")
+		if err != nil {
+			return err
+		}
+
+		s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
+			Type:        "rich",
+			Title:       "Player Profile",
+			Description: "This is a simple player profile.",
+			URL:         "https://www.callofduty.com/",
+			Timestamp:   time.Now().Format(time.RFC3339),
+			Color:       16105282,
+			Author: &discordgo.MessageEmbedAuthor{
+				URL:     "https://www.callofduty.com/",
+				Name:    "Call of Duty",
+				IconURL: "https://www.callofduty.com/content/dam/atvi/callofduty/hub/touch-ui/common/kronos-icon.png",
+			},
+			Thumbnail: &discordgo.MessageEmbedThumbnail{
+				URL: "https://modernwarfarediscordbot.com/images/gamemodes/br.png",
+			},
+			Image: &discordgo.MessageEmbedImage{
+				URL: "https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/kronos/buy/bundle-features/mw-inc-wz-desktop.jpg",
+			},
+			Fields: []*discordgo.MessageEmbedField{
+				{
+					Name:   "Username",
+					Value:  playerProfile.Username,
+					Inline: true,
+				},
+				{
+					Name:   "Platform",
+					Value:  playerProfile.Platform,
+					Inline: true,
+				},
+				{
+					Name:   "Level",
+					Value:  strconv.Itoa(int(playerProfile.Level)),
+					Inline: true,
+				},
+				{
+					Name:   "Total XP",
+					Value:  strconv.Itoa(int(playerProfile.TotalXp)),
+					Inline: true,
+				},
+			},
+			Footer: &discordgo.MessageEmbedFooter{
+				IconURL: "https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/hub/wtb-portal/touts/battlepass-touts/Call-of-Duty_codpoints-coin.png",
+				Text:    "Season Four Reloaded: New Modes and Content Available!",
+			},
+		})
+		return nil
+	}
+
+	return errors.New("Unable to connect to Call of Duty APIs")
 }
