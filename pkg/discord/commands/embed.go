ffc6a11fff0631af753f7664f3151c84c1f9bb64
Keith Craley <kcral21@gmail.com>
2020-07-27 00:18:21 -0400

    update embedded message test with additional features

--- pkg/discord/commands/embed.go
+++ pkg/discord/commands/embed.go
@@ -23,12 +23,34 @@ func embedCmdFunc(s *discordgo.Session, m *discordgo.Message) error {
 		Title:     "embed",
 		Timestamp: time.Now().Format(time.RFC3339),
 		Color:     0xff0000,
+		Author: &discordgo.MessageEmbedAuthor{
+			URL:     "https://github.com/kcraley/octane-go",
+			Name:    "Octane",
+			IconURL: "https://raw.githubusercontent.com/kcraley/octane-go/callofduty/docs/images/yellow-fire-button-medium.png",
+		},
+		Image: &discordgo.MessageEmbedImage{
+			URL: "https://raw.githubusercontent.com/kcraley/octane-go/callofduty/docs/images/yellow-fire-button-medium.png",
+		},
 		Fields: []*discordgo.MessageEmbedField{
 			{
-				Name:   "Message",
-				Value:  "```This is a test embedded message.```",
+				Name:   "Line messgae",
+				Value:  "This is a message on a whole line.",
 				Inline: false,
 			},
+			{
+				Name:   "Inline Message 1",
+				Value:  "This is the first inline message",
+				Inline: true,
+			},
+			{
+				Name:   "Inline Message 2",
+				Value:  "This is the second inline message",
+				Inline: true,
+			},
+		},
+		Footer: &discordgo.MessageEmbedFooter{
+			IconURL: "https://raw.githubusercontent.com/kcraley/octane-go/callofduty/docs/images/yellow-fire-button-medium.png",
+			Text:    "Octane Discord Bot",
 		},
 	}); err != nil {
 		return err
